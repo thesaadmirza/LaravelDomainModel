409ba0e520c928a87f228c24823e7be0b2fc88c1
Saad Mirza <saadmirza009@gmail.com>
2018-02-24 16:46:17 +0500

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+	return view('welcome');
+});
+
+
+Route::group(['namespace'=>'Auth\Controllers'],function(){
+	// Authentication Routes...
+	Route::get('login', 'LoginController@showLoginForm')->name('login');
+	Route::post('login', 'LoginController@login');
+	Route::post('logout', 'LoginController@logout')->name('logout');
+	Route::get('register', 'RegisterController@showRegistrationForm')->name('register');
+	Route::post('register', 'RegisterController@register');
+
+        // Password Reset Routes...
+	Route::get('password/reset', 'ForgotPasswordController@showLinkRequestForm')->name('password.request');
+	Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+	Route::get('password/reset/{token}', 'ResetPasswordController@showResetForm')->name('password.reset');
+	Route::post('password/reset', 'ResetPasswordController@reset');
+});
+        // Registration Routes...
+
+
+Route::get('/home', 'Home\Controllers\HomeController@index')->name('home');
